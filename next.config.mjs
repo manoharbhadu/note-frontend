0dcda00262ef5d625d6299f808d94ce12046ca9a
manohar1780 <bhadu3217@gmail.com>
2025-03-09 17:29:57 +0530

    update url

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BASE_API_URL:  "http://localhost:5000/api/notes"
+        BASE_API_URL:  "https://note-backend-mu1d.onrender.com/api/notes"
     }
 };
 
